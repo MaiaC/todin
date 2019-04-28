851a633fd14ac5771329576d92a01786b29dfcae
Maia Coghlan <maiaccog@gmail.com>
2019-04-28 12:52:27 +0800

    remove devtool from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  devtool: 'eval-source-map',
+  // devtool: 'eval-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
